0b8d7d44494803d7856787606bf00cbb214afaff
David Castro <logophile@google.com>
2026-02-01 15:36:48 -0500

    Debug: Trace shift end times

--- client/src/pages/provider/CreateOpportunityPage.jsx
+++ client/src/pages/provider/CreateOpportunityPage.jsx
@@ -99,7 +99,6 @@ export default function CreateOpportunityPage() {
         const shiftToRemove = shifts.find(s => s.id === id);
         // If it's a real shift (not new_), mark for deletion
         if (shiftToRemove && !shiftToRemove.id.toString().startsWith('new_')) {
-            console.log("Marking shift for deletion:", id);
             setDeletedShiftIds([...deletedShiftIds, id]);
         }
         setShifts(shifts.filter(s => s.id !== id));
@@ -180,10 +179,8 @@ export default function CreateOpportunityPage() {
             }
 
             // Process Deletions
-            console.log("Processing deletions for IDs:", deletedShiftIds);
             if (deletedShiftIds.length > 0) {
                 await Promise.all(deletedShiftIds.map(shiftId => {
-                    console.log("Deleting shift:", shiftId);
                     return pb.collection('shifts').delete(shiftId);
                 }));
             }
